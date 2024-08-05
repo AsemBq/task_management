5f89a349b7e2d067090fcb1cee89551595375bf2
AsemBq <asem.b7460611@gmail.com>
2024-08-05 15:50:51 +0330

    WIP on feature/Router
    [feat] add router

--- src/LogOut/LogOut.jsx
+++ src/LogOut/LogOut.jsx
@@ -1,13 +1,16 @@
 import LogOut from './logout.svg';
 import './LogOut.css'
+import {NavLink} from "react-router-dom";
 export default function LogOutIcon(width, height) {
     return (
-        <img
-            className="logout-icon"
-            src={LogOut}
-            alt="LogOut Icon"
-            width={width}
-            height={height}
-        ></img>
+        <NavLink to={'login'}>
+            <img
+                className="logout-icon"
+                src={LogOut}
+                alt="LogOut Icon"
+                width={width}
+                height={height}
+            ></img>
+        </NavLink>
     );
 }
