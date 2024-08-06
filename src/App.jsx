167e499c1a1a2c05995dc39e194e64ecb33a1a5f
pexicade <itismmd@gmail.com>
2024-08-06 11:58:28 +0330

    Reloved a conflix in wrapper.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './assets/css/App.css';
 import Wrapper from './Wrapper/Wrapper.jsx';
+import {BrowserRouter} from "react-router-dom";
 // import TaskList1 from './TaskList1.jsx'
 
 function App() {
   return (
     <>
-      <Wrapper />
+        <BrowserRouter>
+            <Wrapper />
+        </BrowserRouter>
     </>
   );
 }
--- src/Wrapper/Wrapper.jsx
+++ src/Wrapper/Wrapper.jsx
@@ -7,16 +7,41 @@ import Login from '../Login/Login.jsx';
 
 import './Wrapper.css';
 import EditTask from '../EditTask/EditTask.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Tasks from '../Tasks/Tasks.jsx';
 
 export default function Wrapper() {
   return (
     <div className="wrapper">
-      <Login className="wrapper__item login_page" />
-      <TaskList1 className="wrapper__item TaskList1" />
-      <EmptyTaskList className="wrapper__item EmptyTaskList" />
-      <EditTask className="wrapper__item edit_task" />
-      <CreateTask className="wrapper__item create_task" />
-      <TaskList2 className="wrapper__item TasksList" />
+      <Routes>
+        <Route
+          path="/list"
+          element={<TaskList1 className="wrapper__item TaskList1" />}
+        />
+        <Route
+          path="/create"
+          element={<CreateTask className="wrapper__item create_task" />}
+        />
+        <Route
+          path="/login"
+          element={<Login className="wrapper__item login_page" />}
+        />
+        <Route
+          path="/edit"
+          element={<EditTask className="wrapper__item edit_task" />}
+        />
+        <Route
+          path="*"
+          element={<TaskList2 className="wrapper__item TasksList" />}
+        />
+      </Routes>
+      {/*<Login className="wrapper__item login_page" />*/}
+      {/*<TaskList1 className="wrapper__item TaskList1" />*/}
+      {/*<EmptyTaskList className="wrapper__item EmptyTaskList" />*/}
+      {/*/!* <WrapperItem className="wrapper__item edit_task" /> *!/*/}
+      {/*<EditTask className="wrapper__item edit_task" />*/}
+      {/*<CreateTask className="wrapper__item create_task" />*/}
+      {/*<TaskList2 className="wrapper__item TasksList" />*/}
     </div>
   );
 }
