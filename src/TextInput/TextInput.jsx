8c45d27d4a0c8e05ebae9bde4c232813e6584148
pexicade <itismmd@gmail.com>
2024-08-10 21:12:12 +0330

    [fix] merge resulted in erroneous TextInput.jsx. fixed it

--- src/TextInput/TextInput.jsx
+++ src/TextInput/TextInput.jsx
@@ -1,29 +1,16 @@
 import './TextInput.css';
-import {forwardRef, useRef} from "react";
+import { forwardRef, useRef } from 'react';
 
-const TextInput=({ ...config },ref)=>{
+const TextInput = ({ ...config }, ref) => {
   return (
     <div {...config['parentDiv']['tag']}>
       <label {...config['label']['tag']}> {...config['label']['text']}</label>
       <input ref={ref} {...config['input']['tag']} />
-      </div>
-      )
-export default function TextInput({ errorMessage, ...config }) {
-  const cn = config['parentDiv']['tag']['className'];
-  const error = config['error'];
-  return (
-    <div
-      {...config['parentDiv']['tag']}
-      className={error ? `${cn} erroneous_input` : cn}
-    >
-      <label {...config['label']['tag']}>{config['label']['text']}</label>
-      <input {...config['input']['tag']} />
       <div className="_text-input__icon" {...config['icon']['tag']}>
         {config['icon']['icon']}
       </div>
-      {error && <span className="error-message">{error}</span>}
     </div>
   );
-}
+};
 
 export default forwardRef(TextInput);
