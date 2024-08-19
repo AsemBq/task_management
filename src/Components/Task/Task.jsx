a54d852ef2b1b5fec54d6eb58e42a90462e6f635
pexicade <itismmd@gmail.com>
2024-08-19 11:51:11 +0330

    we can now toggle the done property of the tasks and the changes get applied in redux state

--- src/Components/Task/Task.jsx
+++ src/Components/Task/Task.jsx
@@ -5,16 +5,19 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import useTasks from '../../hooks/useTasks';
 
+import { useDispatch } from 'react-redux';
+import { toggleDone } from '../../Slices/taskSlice';
+
 const Task = ({ listId, name, isDone }) => {
   const navigate = useNavigate();
-
-  const [isChecked, setIsChecked] = useState(isDone);
+  const dispatch = useDispatch();
 
   const { editTask, getTaskById } = useTasks();
 
   const handleChange = async () => {
-    await editTask(listId, { ...getTaskById(listId), done: !isChecked });
-    setIsChecked(!isChecked);
+    isDone = !isDone;
+    await editTask(listId, { ...getTaskById(listId), done: isDone });
+    dispatch(toggleDone({ id: listId, done: isDone }));
   };
 
   const handleClick = () => {
@@ -27,7 +30,7 @@ const Task = ({ listId, name, isDone }) => {
         <span className="circle">A</span> <span> {name}</span>
       </div>
       <label className="custom-checkbox">
-        <input type="checkbox" checked={isChecked} onChange={handleChange} />
+        <input type="checkbox" checked={isDone} onChange={handleChange} />
         <span className="checkmark"></span>
       </label>
     </li>
